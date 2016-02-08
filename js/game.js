0cde24beb0c313bc70132db6e048eb79580c807b
sam <sam@silver42.karoo.co.uk>
2016-02-08 22:35:43 +0000

    Fix out of date file

--- js/game.js
+++ js/game.js
@@ -1,6 +1,28 @@
 (function (console, $global) { "use strict";
 var $estr = function() { return js_Boot.__string_rec(this,''); };
+function $extend(from, fields) {
+	function Inherit() {} Inherit.prototype = from; var proto = new Inherit();
+	for (var name in fields) proto[name] = fields[name];
+	if( fields.toString !== Object.prototype.toString ) proto.toString = fields.toString;
+	return proto;
+}
+var HxOverrides = function() { };
+HxOverrides.__name__ = true;
+HxOverrides.indexOf = function(a,obj,i) {
+	var len = a.length;
+	if(i < 0) {
+		i += len;
+		if(i < 0) i = 0;
+	}
+	while(i < len) {
+		if(a[i] === obj) return i;
+		i++;
+	}
+	return -1;
+};
 var Main = function() {
+	this.vidHeight = 240;
+	this.vidWidth = 320;
 	this.loaded = false;
 	window.onload = $bind(this,this.onWindowLoaded);
 };
@@ -22,13 +44,13 @@ Main.prototype = {
 			unsupportedInfo.style.color = "#ffffff";
 			switch(glSupported) {
 			case 2:
-				unsupportedInfo.innerHTML = "Your browser does not support WebGL. Click <a href=\"" + "https://github.com/Tw1ddle/Stereoscopics" + "\" target=\"_blank\">here for project info</a> instead.";
+				unsupportedInfo.innerHTML = "Your browser does not support WebGL. Click <a href=\"" + "https://github.com/Tw1ddle/Amsterdam_Light_Festival" + "\" target=\"_blank\">here for project info</a> instead.";
 				break;
 			case 1:
-				unsupportedInfo.innerHTML = "Your browser supports WebGL, but the feature appears to be disabled. Click <a href=\"" + "https://github.com/Tw1ddle/Stereoscopics" + "\" target=\"_blank\">here for project info</a> instead.";
+				unsupportedInfo.innerHTML = "Your browser supports WebGL, but the feature appears to be disabled. Click <a href=\"" + "https://github.com/Tw1ddle/Amsterdam_Light_Festival" + "\" target=\"_blank\">here for project info</a> instead.";
 				break;
 			default:
-				unsupportedInfo.innerHTML = "Could not detect WebGL support. Click <a href=\"" + "https://github.com/Tw1ddle/Stereoscopics" + "\" target=\"_blank\">here for project info</a> instead.";
+				unsupportedInfo.innerHTML = "Could not detect WebGL support. Click <a href=\"" + "https://github.com/Tw1ddle/Amsterdam_Light_Festival" + "\" target=\"_blank\">here for project info</a> instead.";
 			}
 			gameDiv.appendChild(unsupportedInfo);
 			return;
@@ -43,7 +65,7 @@ Main.prototype = {
 			missingExtensionInfo.style.width = "100%";
 			missingExtensionInfo.style.textAlign = "center";
 			missingExtensionInfo.style.color = "#ffffff";
-			missingExtensionInfo.innerHTML = "Missing required WebGL extension: " + extDerivatives + " Click <a href=\"" + "https://github.com/Tw1ddle/Stereoscopics" + "\" target=\"_blank\">here for project info</a> instead.";
+			missingExtensionInfo.innerHTML = "Missing required WebGL extension: " + extDerivatives + " Click <a href=\"" + "https://github.com/Tw1ddle/Amsterdam_Light_Festival" + "\" target=\"_blank\">here for project info</a> instead.";
 			gameDiv.appendChild(missingExtensionInfo);
 			return;
 		}
@@ -68,18 +90,29 @@ Main.prototype = {
 		this.scene = new THREE.Scene();
 		this.camera = new THREE.PerspectiveCamera(75,width / height,1.0,8000.0);
 		this.camera.position.z = 150;
-		this.videoImageElement = window.document.getElementById("monitor");
-		this.videoImageCanvas = window.document.getElementById("videoImage");
-		this.videoImageContext = this.videoImageCanvas.getContext("2d");
-		this.videoImageContext.fillStyle = "#000000";
-		this.videoImageContext.fillRect(0,0,this.videoImageCanvas.width,this.videoImageCanvas.height);
-		this.videoTexture = new THREE.Texture(this.videoImageCanvas);
+		this.scene.add(this.camera);
+		var _this = window.document;
+		this.videoElement = _this.createElement("video");
+		this.videoElement.width = this.vidWidth;
+		this.videoElement.height = this.vidHeight;
+		this.videoElement.autoplay = true;
+		this.videoElement.loop = true;
+		var nav = navigator;
+		if(!(nav != null)) throw new js__$Boot_HaxeError("FAIL: nav != null");
+		this.windowUrl = window.URL || window.webkitURL;
+		if(!(this.windowUrl != null)) throw new js__$Boot_HaxeError("FAIL: windowUrl != null");
+		nav.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+		if(!(nav.getUserMedia != null)) throw new js__$Boot_HaxeError("FAIL: nav.getUserMedia != null");
+		nav.getUserMedia({ video : true},$bind(this,this.webcamLoadSuccess),$bind(this,this.webcamLoadError));
+		this.videoTexture = new THREE.Texture(this.videoElement);
 		this.videoTexture.minFilter = THREE.LinearFilter;
 		this.videoTexture.magFilter = THREE.LinearFilter;
-		var material = new THREE.MeshBasicMaterial({ map : this.videoTexture, overdraw : 1, side : THREE.DoubleSide});
+		var material = new THREE.ShaderMaterial({ vertexShader : shaders_Dazzle.vertexShader, fragmentShader : shaders_Dazzle.fragmentShader, uniforms : shaders_Dazzle.uniforms, opacity : 1, side : THREE.DoubleSide});
+		shaders_Dazzle.uniforms.tDiffuse.value = this.videoTexture;
+		shaders_Dazzle.uniforms.resolution.value.set(this.vidWidth,this.vidHeight);
+		shaders_Dazzle.uniforms.checkSize.value = 20.0;
 		var geometry = new THREE.PlaneGeometry(100,100,1,1);
 		var screen = new THREE.Mesh(geometry,material);
-		screen.position.set(0,0,0);
 		this.scene.add(screen);
 		this.camera.lookAt(screen.position);
 		this.composer = new THREE.EffectComposer(this.renderer);
@@ -111,6 +144,12 @@ Main.prototype = {
 		gameDiv.appendChild(this.renderer.domElement);
 		window.requestAnimationFrame($bind(this,this.animate));
 	}
+	,webcamLoadSuccess: function(stream) {
+		this.videoElement.src = this.windowUrl.createObjectURL(stream);
+	}
+	,webcamLoadError: function(error) {
+		null;
+	}
 	,onResize: function() {
 		var width = window.innerWidth * this.renderer.getPixelRatio();
 		var height = window.innerHeight * this.renderer.getPixelRatio();
@@ -123,10 +162,7 @@ Main.prototype = {
 	,animate: function(time) {
 		Main.dt = (time - Main.lastAnimationTime) * 0.001;
 		Main.lastAnimationTime = time;
-		if(this.videoImageElement.readyState == 4) {
-			this.videoImageContext.drawImage(this.videoImageElement,0,0,this.videoImageCanvas.width,this.videoImageCanvas.height);
-			if(this.videoTexture != null) this.videoTexture.needsUpdate = true;
-		}
+		if(this.videoElement.readyState == 4) this.videoTexture.needsUpdate = true;
 		this.composer.render(Main.dt);
 		window.requestAnimationFrame($bind(this,this.animate));
 	}
@@ -139,6 +175,7 @@ Reflect.field = function(o,field) {
 	try {
 		return o[field];
 	} catch( e ) {
+		if (e instanceof js__$Boot_HaxeError) e = e.val;
 		return null;
 	}
 };
@@ -146,10 +183,45 @@ Reflect.getProperty = function(o,field) {
 	var tmp;
 	if(o == null) return null; else if(o.__properties__ && (tmp = o.__properties__["get_" + field])) return o[tmp](); else return o[field];
 };
+Reflect.fields = function(o) {
+	var a = [];
+	if(o != null) {
+		var hasOwnProperty = Object.prototype.hasOwnProperty;
+		for( var f in o ) {
+		if(f != "__id__" && f != "hx__closures__" && hasOwnProperty.call(o,f)) a.push(f);
+		}
+	}
+	return a;
+};
 var Std = function() { };
 Std.__name__ = true;
 Std.string = function(s) {
 	return js_Boot.__string_rec(s,"");
+};
+var dat_ShaderGUI = function() { };
+dat_ShaderGUI.__name__ = true;
+dat_ShaderGUI.generate = function(gui,folderName,uniforms,exclude) {
+	var keys = Reflect.fields(uniforms);
+	var folder = gui.addFolder(folderName);
+	var _g = 0;
+	while(_g < keys.length) {
+		var key = keys[_g];
+		++_g;
+		var v = Reflect.getProperty(uniforms,key);
+		if(exclude != null && HxOverrides.indexOf(exclude,key,0) != -1) continue;
+		var type = v.type;
+		var value = v.value;
+		switch(type) {
+		case "f":
+			if(Object.prototype.hasOwnProperty.call(v,"min") && Object.prototype.hasOwnProperty.call(v,"max")) folder.add(v,"value").listen().min(v.min).max(v.max).name(key); else folder.add(v,"value").listen().name(key);
+			break;
+		case "v2":
+			var f = folder.addFolder(key);
+			f.add(v.value,"x").listen().name(key + "_x");
+			f.add(v.value,"y").listen().name(key + "_y");
+			break;
+		}
+	}
 };
 var dat_ThreeObjectGUI = function() { };
 dat_ThreeObjectGUI.__name__ = true;
@@ -188,6 +260,17 @@ dat_ThreeObjectGUI.addItem = function(gui,object,tag) {
 	}
 	return folder;
 };
+var js__$Boot_HaxeError = function(val) {
+	Error.call(this);
+	this.val = val;
+	this.message = String(val);
+	if(Error.captureStackTrace) Error.captureStackTrace(this,js__$Boot_HaxeError);
+};
+js__$Boot_HaxeError.__name__ = true;
+js__$Boot_HaxeError.__super__ = Error;
+js__$Boot_HaxeError.prototype = $extend(Error.prototype,{
+	__class__: js__$Boot_HaxeError
+});
 var js_Boot = function() { };
 js_Boot.__name__ = true;
 js_Boot.getClass = function(o) {
@@ -235,6 +318,7 @@ js_Boot.__string_rec = function(o,s) {
 		try {
 			tostr = o.toString;
 		} catch( e ) {
+			if (e instanceof js__$Boot_HaxeError) e = e.val;
 			return "???";
 		}
 		if(tostr != null && tostr != Object.toString && typeof(tostr) == "function") {
@@ -321,12 +405,17 @@ js_Boot.__isNativeObj = function(o) {
 js_Boot.__resolveNativeClass = function(name) {
 	return $global[name];
 };
+var shaders_Dazzle = function() { };
+shaders_Dazzle.__name__ = true;
 var shaders_FXAA = function() { };
 shaders_FXAA.__name__ = true;
 var util_FileReader = function() { };
 util_FileReader.__name__ = true;
 var $_, $fid = 0;
 function $bind(o,m) { if( m == null ) return null; if( m.__id__ == null ) m.__id__ = $fid++; var f; if( o.hx__closures__ == null ) o.hx__closures__ = {}; else f = o.hx__closures__[m.__id__]; if( f == null ) { f = function(){ return f.method.apply(f.scope, arguments); }; f.scope = o; f.method = m; o.hx__closures__[m.__id__] = f; } return f; }
+if(Array.prototype.indexOf) HxOverrides.indexOf = function(a,o,i) {
+	return Array.prototype.indexOf.call(a,o,i);
+};
 String.prototype.__class__ = String;
 String.__name__ = true;
 Array.__name__ = true;
@@ -338,7 +427,7 @@ var Bool = Boolean;
 Bool.__ename__ = ["Bool"];
 var Class = { __name__ : ["Class"]};
 var Enum = { };
-Main.REPO_URL = "https://github.com/Tw1ddle/Stereoscopics";
+Main.REPO_URL = "https://github.com/Tw1ddle/Amsterdam_Light_Festival";
 Main.WEBSITE_URL = "http://samcodes.co.uk/";
 Main.TWITTER_URL = "https://twitter.com/Sam_Twidale";
 Main.HAXE_URL = "http://haxe.org/";
@@ -346,6 +435,9 @@ Main.lastAnimationTime = 0.0;
 Main.dt = 0.0;
 dat_ThreeObjectGUI.guiItemCount = 0;
 js_Boot.__toStr = {}.toString;
+shaders_Dazzle.uniforms = { tDiffuse : { type : "t", value : null}, resolution : { type : "v2", value : new THREE.Vector2(1024.0,1024.0)}, checkSize : { type : "f", value : 20.0}};
+shaders_Dazzle.vertexShader = "varying vec2 vUv;\r\n\r\nvoid main()\r\n{\r\n\tvUv = uv;\r\n\tgl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);\r\n}";
+shaders_Dazzle.fragmentShader = "varying vec2 vUv;\r\n\r\nuniform sampler2D tDiffuse;\r\nuniform vec2 resolution;\r\nuniform float checkSize;\r\n\r\nvec3 checker(vec2 uv)\r\n{\r\n\tfloat fm = mod(floor(checkSize * uv.x) + floor(checkSize * uv.y), 2.0);\r\n\r\n\tif (fm < 1.0)\r\n\t{\r\n\t\treturn vec3(1.0, 1.0, 1.0);\r\n\t} \r\n\telse\r\n\t{\r\n\t\treturn vec3(0.0, 0.0, 0.0);\r\n\t}\r\n}\r\n\r\nvoid main()\r\n{\r\n\tvec4 color = texture2D(tDiffuse, vUv);\r\n\tvec3 check = checker(vUv);\r\n\tgl_FragColor = mix(vec4(check, 1.0), color, 0.5);\r\n}";
 shaders_FXAA.uniforms = { tDiffuse : { type : "t", value : null}, resolution : { type : "v2", value : new THREE.Vector2(1024.0,1024.0)}};
 shaders_FXAA.vertexShader = "varying vec2 vUv;\r\n\r\nvoid main()\r\n{\r\n\tvUv = uv;\r\n\tgl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);\r\n}";
 shaders_FXAA.fragmentShader = "// Fast approximate anti-aliasing shader\r\n// Based on the three.js implementation: https://github.com/mrdoob/three.js/blob/master/examples/js/shaders/FXAAShader.js\r\n// Ported to three.js by alteredq: http://alteredqualia.com/ and davidedc: http://www.sketchpatch.net/\r\n// Ported to WebGL by @supereggbert: http://www.geeks3d.com/20110405/fxaa-fast-approximate-anti-aliasing-demo-glsl-opengl-test-radeon-geforce/\r\n// Originally implemented as NVIDIA FXAA by Timothy Lottes: http://timothylottes.blogspot.com/2011/06/fxaa3-source-released.html\r\n// Paper: http://developer.download.nvidia.com/assets/gamedev/files/sdk/11/FXAA_WhitePaper.pdf\r\n\r\n#define FXAA_REDUCE_MIN (1.0/128.0)\r\n#define FXAA_REDUCE_MUL (1.0/8.0)\r\n#define FXAA_SPAN_MAX 8.0\r\n\r\nvarying vec2 vUv;\r\n\r\nuniform sampler2D tDiffuse;\r\nuniform vec2 resolution;\r\n\r\nvoid main()\r\n{\r\n\tvec2 rres = vec2(1.0) / resolution;\r\n\t\r\n\t// Texture lookups to find RGB values in area of current fragment\r\n\tvec3 rgbNW = texture2D(tDiffuse, (gl_FragCoord.xy + vec2(-1.0, -1.0)) * rres).xyz;\r\n\tvec3 rgbNE = texture2D(tDiffuse, (gl_FragCoord.xy + vec2(1.0, -1.0)) * rres).xyz;\r\n\tvec3 rgbSW = texture2D(tDiffuse, (gl_FragCoord.xy + vec2(-1.0, 1.0)) * rres).xyz;\r\n\tvec3 rgbSE = texture2D(tDiffuse, (gl_FragCoord.xy + vec2(1.0, 1.0)) * rres).xyz;\r\n\tvec4 rgbaM = texture2D(tDiffuse, gl_FragCoord.xy  * rres);\r\n\tvec3 rgbM = rgbaM.xyz;\r\n\tfloat opacity = rgbaM.w;\r\n\t\r\n\t// Luminance estimates for colors around current fragment\r\n\tvec3 luma = vec3(0.299, 0.587, 0.114);\r\n\tfloat lumaNW = dot(rgbNW, luma);\r\n\tfloat lumaNE = dot(rgbNE, luma);\r\n\tfloat lumaSW = dot(rgbSW, luma);\r\n\tfloat lumaSE = dot(rgbSE, luma);\r\n\tfloat lumaM  = dot(rgbM, luma);\r\n\tfloat lumaMin = min(lumaM, min(min(lumaNW, lumaNE), min(lumaSW, lumaSE)));\r\n\tfloat lumaMax = max(lumaM, max(max(lumaNW, lumaNE), max(lumaSW, lumaSE)));\r\n\r\n\t// \r\n\tvec2 dir;\r\n\tdir.x = -((lumaNW + lumaNE) - (lumaSW + lumaSE));\r\n\tdir.y =  ((lumaNW + lumaSW) - (lumaNE + lumaSE));\r\n\r\n\tfloat dirReduce = max((lumaNW + lumaNE + lumaSW + lumaSE) * (0.25 * FXAA_REDUCE_MUL), FXAA_REDUCE_MIN);\r\n\r\n\tfloat rcpDirMin = 1.0 / (min(abs(dir.x), abs(dir.y)) + dirReduce);\r\n\tdir = min(vec2(FXAA_SPAN_MAX, FXAA_SPAN_MAX), max(vec2(-FXAA_SPAN_MAX, -FXAA_SPAN_MAX), dir * rcpDirMin)) * rres;\r\n\r\n\tvec3 rgbA = 0.5 * (texture2D(tDiffuse, gl_FragCoord.xy * rres + dir * (1.0 / 3.0 - 0.5 )).xyz + texture2D(tDiffuse, gl_FragCoord.xy * rres + dir * (2.0 / 3.0 - 0.5)).xyz);\r\n\tvec3 rgbB = rgbA * 0.5 + 0.25 * (texture2D(tDiffuse, gl_FragCoord.xy * rres + dir * -0.5).xyz + texture2D(tDiffuse, gl_FragCoord.xy * rres + dir * 0.5).xyz);\r\n\r\n\tfloat lumaB = dot(rgbB, luma);\r\n\t\r\n\tif ((lumaB < lumaMin) || (lumaB > lumaMax))\r\n\t{\r\n\t\tgl_FragColor = vec4(rgbA, opacity);\r\n\t}\r\n\telse\r\n\t{\r\n\t\tgl_FragColor = vec4(rgbB, opacity);\r\n\t}\r\n}";
